410dbde8a114d7a0b5079fa0ab5c11fa08c9c042
mclilzee <mc.lilzee@hotmail.com>
2022-05-30 04:33:20 +0200

    Create Homepage component

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Homepage(props) {
+
+  return (
+    <div className={"homepage"}>
+      <h1>Pokemon Center</h1>
+      <h3>Welcome to pokemon center traveler!</h3>
+      <p>Pick and choose a pokemon to purchase, they are all free, buy as many as you would like without paying a cent.
+        Isn't this a perfect world that you wish you live in?
+      </p>
+    </div>
+  );
+}
+
+export default Homepage;
