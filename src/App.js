e8edccda212a4f33b38696c3d5c0a59a0f7ac95c
mclilzee <mc.lilzee@hotmail.com>
2022-05-30 04:33:33 +0200

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import Homepage from "./components/Homepage/Homepage";
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Homepage/>
     </div>
   );
 }
