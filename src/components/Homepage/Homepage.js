f4261ed8601887e8ea5c4650686bf34d53237255
mclilzee <mc.lilzee@hotmail.com>
2022-05-30 13:27:32 +0200

    Seperate homepage css file

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./homepage.css";
 
 function Homepage(props) {
 
@@ -6,7 +7,8 @@ function Homepage(props) {
     <div className={"homepage"}>
       <h1>Pokémon Center</h1>
       <h3>Welcome to Pokémon center traveler!</h3>
-      <p data-testid={"description"}>Pick and choose a pokemon to purchase, they are all free, buy as many as you would like without paying a cent.
+      <p data-testid={"description"}>Pick and choose a pokemon to purchase, they are all free, buy as many as you would
+        like without paying a cent.
         Isn't this a perfect world that you wish you live in?
       </p>
     </div>
