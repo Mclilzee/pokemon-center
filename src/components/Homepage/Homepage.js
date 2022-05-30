4d6ad7752575cd56cc6b15d7ad6f73d7ebca8164
mclilzee <mc.lilzee@hotmail.com>
2022-05-30 13:26:09 +0200

    Create Homepage test suite

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -4,9 +4,9 @@ function Homepage(props) {
 
   return (
     <div className={"homepage"}>
-      <h1>Pokemon Center</h1>
-      <h3>Welcome to pokemon center traveler!</h3>
-      <p>Pick and choose a pokemon to purchase, they are all free, buy as many as you would like without paying a cent.
+      <h1>Pokémon Center</h1>
+      <h3>Welcome to Pokémon center traveler!</h3>
+      <p data-testid={"description"}>Pick and choose a pokemon to purchase, they are all free, buy as many as you would like without paying a cent.
         Isn't this a perfect world that you wish you live in?
       </p>
     </div>
