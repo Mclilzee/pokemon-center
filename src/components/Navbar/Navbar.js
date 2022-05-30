14851a13a910fcf1f891d5bc3a86d1e8ba958b04
mclilzee <mc.lilzee@hotmail.com>
2022-05-30 03:35:42 +0200

    Create navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar(props) {
+
+  return (
+    <div className={"navbar"}>
+      <h2>Home</h2>
+      <h2>Store</h2>
+      <h2>Cart</h2>
+    </div>
+  )
+}
