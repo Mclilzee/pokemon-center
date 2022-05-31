6f2345442130075438eac7086db863ad686ea2fb
mclilzee <mc.lilzee@hotmail.com>
2022-05-31 05:30:00 +0200

    Capitalize names

--- src/components/Storepage/PokemonCard.js
+++ src/components/Storepage/PokemonCard.js
@@ -15,6 +15,14 @@ function PokemonCard(props) {
     setPokemon(item);
   }
 
+  function capitalize(text) {
+    if (text.length < 1) {
+      return;
+    }
+
+    return text[0].toUpperCase() + text.slice(1);
+  }
+
   function loadPokemon() {
     if (pokemon === null) {
       return <h1>Loading...</h1>;
@@ -23,7 +31,7 @@ function PokemonCard(props) {
     return (
       <>
         <img src={pokemon.sprites.other["official-artwork"]["front_default"]} alt={`Image of ${pokemon.name}`}/>
-        <h2>{pokemon.name}</h2>
+        <h2>{capitalize(pokemon.name)}</h2>
       </>
     );
   }
