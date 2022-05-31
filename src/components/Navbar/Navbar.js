a380b8f5017b7eaf10188113446ab2ea3e846369
mclilzee <mc.lilzee@hotmail.com>
2022-05-31 03:33:38 +0200

    Narbar component refactoring

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Navbar(props) {
 
   return (
-    <div className={"navbar"}>
+    <nav className={"navbar"}>
       <h2>Home</h2>
       <h2>Store</h2>
       <h2>Cart</h2>
-    </div>
+    </nav>
   )
 }
