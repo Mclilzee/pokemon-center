c30c406158c2cc2c0e140dc2e1ca2339f06d1782
mclilzee <mc.lilzee@hotmail.com>
2022-05-31 13:27:38 +0200

    Createa page arrows svg

--- src/components/Storepage/Page.js
+++ src/components/Storepage/Page.js
@@ -5,7 +5,9 @@ function Page(props) {
   return (
     <div className={"page-number-container"}>
       <img className="previous-page-arrow" src={"./assets/icons/arrow_previous.svg"} alt={"previous arrow"}/>
-      <input type={"number"} value={props.pageNumber}/>
+      <div className={"previous-page-number"}>{props.pageNumber - 1}</div>
+      <div className={"page-number"}>{props.pageNumber}</div>
+      <div className={"next-page-number"}>{props.pageNumber + 1}</div>
       <img className={"next-page-arrow"} src={"./assets/icons/arrow_next.svg"} alt={"next arrow"}/>
     </div>
   );
