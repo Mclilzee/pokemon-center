7c8528e786f2910822233f2ef05b158af682b5a8
mclilzee <mc.lilzee@hotmail.com>
2022-06-02 01:09:35 +0200

    Add test suite for Pokemon component

---
+++ src/components/Storepage/Pokemon.test.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Pokemon from "./Pokemon";
+import pokemon from "./Pokemon";
+
+describe("Includes all render elements", () => {
+  test("Renders correctly", () => {
+  });
+
+  beforeEach(() => {
+    render(
+      <Pokemon
+        name={"bulbasaur"}
+        type={["grass", "poison"]}
+        img={"mockImageURL"}
+      />
+    );
+  });
+
+  test("Pokemon name on screen", () => {
+    const pokemonName = screen.getByText("bulbasaur");
+    expect(pokemonName).toBeInTheDocument();
+  });
+
+  test("Pokemon type on screen", () => {
+    const pokemonType = screen.getByTestId("type-test");
+    expect(pokemonType).toBeInTheDocument();
+  });
+
+  test("Pokemon display correct type", () => {
+    const pokemonType = screen.getByTestId("type-test");
+    expect(pokemonType.textContent).toBe("grasspoison");
+  });
+
+  test("Pokemon image display correctly", () => {
+    const pokemonImage = screen.getByAltText("bulbasaur");
+    expect(pokemonImage).toBeInTheDocument();
+  });
+
+});
+
+test("Test for placeholder image", () => {
+
+  render(
+    <Pokemon
+      name={"bulbasaur"}
+      type={["grass", "poison"]}
+      img={null}
+    />
+  );
+
+  const pokmeonImage = screen.getByAltText("bulbasaur");
+  expect(pokmeonImage).toBeInTheDocument();
+});
+
