6c4e981175746d920bb754751a6fe85d6e02b324
mclilzee <mc.lilzee@hotmail.com>
2022-06-04 10:58:01 +0200

    Modify Navbar component test suite

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -2,32 +2,36 @@ import React from "react";
 import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Navbar from "./Navbar";
+import { act } from "react-dom/test-utils";
+import { BrowserRouter, Router } from "react-router-dom";
 
 describe("Navbar renders correctly", () => {
 
+  beforeEach(() => {
+    act(() => {
+      render(
+        <BrowserRouter>
+          <Navbar/>)
+        </BrowserRouter>
+      );
+    });
+  });
+
   test("Navbar renders", () => {
-    render(<Navbar/>);
   });
 
   test("Navbar home link in document", () => {
-    render(<Navbar/>);
-
     const homepageLink = screen.getByText("Home");
     expect(homepageLink).toBeInTheDocument();
   });
 
   test("Navbar store link in document", () => {
-    render(<Navbar/>);
-
     const homepageLink = screen.getByText("Cart");
     expect(homepageLink).toBeInTheDocument();
   });
 
   test("Navbar cart link in document", () => {
-    render(<Navbar/>);
-
     const homepageLink = screen.getByText("Store");
     expect(homepageLink).toBeInTheDocument();
   });
-
 });
