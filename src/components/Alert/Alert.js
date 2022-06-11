066976849be7fc47d482545a52719fca78665d4d
Mclilzee <mc.lilzee@hotmail.com>
2022-06-11 20:10:39 +0200

    Add styling to Alert component

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,6 +1,7 @@
 import React from "react";
 import completedIcon from "../../assets/icons/completed.svg";
 import errorIcon from "../../assets/icons/error.svg";
+import "./alert.css";
 
 function Alert(props) {
 
@@ -9,9 +10,11 @@ function Alert(props) {
   }
   return (
     <div className={"alert-container"}>
-      <h1>{props.completed ? "Success!" : "Error"}</h1>
-      <img src={props.completed ? completedIcon : errorIcon} alt={props.completed ? "completed icon" : "error icon"}/>
-      <h2>{props.message}</h2>
+      <div className={"alert"}>
+        <h1 className={"title"}>{props.completed ? "Success!" : "Error"}</h1>
+        <img src={props.completed ? completedIcon : errorIcon} alt={props.completed ? "completed icon" : "error icon"}/>
+        <h2 className={"message"}>{props.message}</h2>
+      </div>
     </div>
   );
 }
