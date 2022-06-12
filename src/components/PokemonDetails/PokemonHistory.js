24673e6ddcd36b18553b17168c2dc4f96f2882d3
Mclilzee <mc.lilzee@hotmail.com>
2022-06-12 23:52:46 +0200

    Fix bug with pokemons that has no history

--- src/components/PokemonDetails/PokemonHistory.js
+++ src/components/PokemonDetails/PokemonHistory.js
@@ -39,11 +39,15 @@ function PokemonHistory(props) {
 
   return (
     <div className={"pokemon-history"}>
-      <h2 className={"flavor-message"}>{history.flavor_text_entries[0].flavor_text}</h2>
-      <div className={"habitat-message"}>
-        <h3 className={"habitat"}>Habitat</h3>
-        <h3 className={"habitat-name"}>{capitalize(history.habitat.name)}</h3>
-      </div>
+      {history.flavor_text_entries !== null &&
+        <h2 className={"flavor-message"}>{history.flavor_text_entries[0].flavor_text}</h2>
+      }
+      {history.habitat !== null &&
+        <div className={"habitat-message"}>
+          <h3 className={"habitat"}>Habitat</h3>
+          <h3 className={"habitat-name"}>{capitalize(history.habitat.name)}</h3>
+        </div>
+      }
       <div data-testid={"evolution-test"} className={"evolution-message"}>
         <h3 className={"evolves-from"}>Evolves from</h3>
         <h3 className={"evolution-name"}>{generateEvolutionLink()}</h3>
