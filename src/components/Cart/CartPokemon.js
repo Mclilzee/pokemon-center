3d5176dc606670ac8ae70fab0555865fb361de44
Mclilzee <mc.lilzee@hotmail.com>
2022-06-13 00:03:31 +0200

    Add elements to CartPokemon component

--- src/components/Cart/CartPokemon.js
+++ src/components/Cart/CartPokemon.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function CartPokemon(props) {
 
   return (
-    <div>
-      <h1>{props.name} {props.amount}</h1>
-      <img src={props.icon} alt={props.name + " icon"}/>
+    <div className={"cart-pokemon"}>
+      <img className={"pokemon-icon"} src={props.icon} alt={props.name + " icon"}/>
+      <Link className={"pokemon-name"} to={`/pokemon/${props.name}`}>
+        {props.name}
+      </Link>
+      <input className={"amount-input"} type={"number"} min={1} max={10} value={props.amount}/>
+      <button className={"remove-button"}>X</button>
     </div>
   );
 }
